6d4cbbb3c0c297be2f56b7593a7fe9ed189db433
Divya <Divya@192.168.0.13>
2016-12-16 12:12:54 -0800

    done

---
+++ PrintName.java
@@ -0,0 +1,12 @@
+package com.divya.oracle;
+
+public class PrintName {
+
+	public static void main(String[] args) {
+		{
+			System.out.println("My little princess is *** Rivya*** ");
+		}
+
+	}
+
+}
