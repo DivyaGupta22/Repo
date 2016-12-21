cc378efc40fc89f9dd2bd5f88c11453f91cc6a17
Divya <Divya@192.168.0.13>
2016-12-20 23:13:18 -0800

    done

---
+++ Multiplication.java
@@ -0,0 +1,22 @@
+package com.divya.oracle;
+
+public class Multiplication {
+	
+	public void mul(int a,int b)
+	{
+		System.out.println("Multiplication of 2 int number :"+ a*b); 
+	}
+
+	public void mul (int a,int b,int c)
+	{
+		System.out.println("Multiplication of 3 int number :"+ a*b*c);
+	
+	}
+	
+	public void mul(int a, float b) 
+{ 
+	System.out.println("Multiplication of int and float number :"+ a*b);	
+}
+
+}
+
---
+++ MultiplicationOverloading.java
@@ -0,0 +1,13 @@
+package com.divya.oracle;
+
+public class MultiplicationOverloading {
+
+	public static void main(String[] args) {
+		Multiplication obj = new Multiplication();
+		obj.mul(10, 5);
+		obj.mul(7,8,9);
+		obj.mul(5, 6.5f);
+		
+	}
+
+}
