115c5fb525b47ab59ae42b4236998e7b3f947034
Divya <Divya@192.168.0.13>
2016-12-20 23:31:23 -0800

    done

---
+++ CountObj.java
@@ -0,0 +1,12 @@
+package com.divya.oracle;
+
+public class CountObj {
+	public static int count=0;
+	CountObj()
+	{ ++count;
+	System.out.println(" Total number of object created :" + count);
+	}
+	
+}
+
+
---
+++ CountObjTest.java
@@ -0,0 +1,14 @@
+package com.divya.oracle;
+
+public class CountObjTest {
+
+	public static void main(String[] args) {
+	 CountObj obj1 = new CountObj();
+	 CountObj obj2 = new CountObj();
+	 CountObj obj3 = new CountObj();
+	 CountObj obj4 = new CountObj();
+	 CountObj obj5 = new CountObj();
+	 CountObj obj6 = new CountObj();
+	}
+
+}
